84a1ab74d0192e578b0508edf34027ade027fa19
Pokemon-Mini <vargrskaoi@gmail.com>
2024-03-20 08:45:19 -0500

    Web Programming 20-3-2024

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+
+const HTTP_PORT = 8000;
+console.log("Listening on port " + HTTP_PORT);
+
+var app = express();
+app.use(cors());
+
+let arrFruit = [];
+
+let objBanana = {name:'banana', color:'yellow'};
+let objApple = {name:'apple', color:'red'};
+let objGrape = {name:'grape', color:'purple'};
+
+arrFruit.push(objBanana);
+arrFruit.push(objApple);
+arrFruit.push(objGrape);
+
+app.get("/", (req,res,next) => {
+    let strFruit = req.query.fruit;
+    console.log("Routed to base route");
+    console.log(strFruit);
+
+    arrFruit.forEach(function(thisFruit){
+        if(thisFruit.name == strFruit){
+            res.status(200).send(thisFruit);
+        }
+    })
+    res.status(200).send('{"message":"Fruit not Found"}');
+})
+
+app.get("/hello", (req,res,next) => {
+    let strFruit = req.query.fruit;
+    console.log("Routed to hello route");
+    console.log(strFruit);
+
+    res.status(200).send("Hello " + strFruit);
+})
+
+app.listen(HTTP_PORT);
