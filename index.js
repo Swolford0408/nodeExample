2b920d628db05c5199164dbbc517d725270ba3b7
Pokemon-Mini Win <vargrskaoi@gmail.com>
2024-03-27 08:48:02 -0500

    27-03-2024

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const {v4:uuidv4} = require('uuid');
 const sqlite3 = require('sqlite3').verbose();
 const dbSource = "node.db";
 const db = new sqlite3.Database(dbSource);
@@ -52,8 +53,8 @@ app.post("/fruit", (req,res,next) => {
     }
 })
 
-app.get("/fruit", (req,res,next) => {
-    let strName = req.query.name;
+app.get("/fruit/:name", (req,res,next) => {
+    let strName = req.params.name;
     if(strName){
         let strCommand = "SELECT * FROM tblFruit WHERE name = ?";
         let arrParameters = [strName];
